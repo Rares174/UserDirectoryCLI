571deade638e86e214088ff4287462bd53031180
Olariu Rares <raresstefanolariu@gmail.com>
2025-07-04 02:03:27 +0300

    v 1.2 fixed menu choice validation, added name validation and email validation(not complete) and fixed removal ID validation

--- src/Main.java
+++ src/Main.java
@@ -22,57 +22,86 @@ public class Main {
         ArrayList<User> Users = new ArrayList<>();
         while(!exit) {
             menu();
-            choice = sc.nextInt();
-            switch (choice) {
-                case (1):
-                    System.out.println("Enter user name: ");
-                    sc.nextLine();
-                    name = sc.nextLine();
-                    System.out.println("Enter user email:");
-                    email = sc.nextLine();
-                    Users.add(new User(++count, name, email));
-                    break;
-                case (2):
-                    if(!Users.isEmpty()) {
-                        for (User user : Users)
-                            System.out.println(user.toString());
-                    }
-                    else{
-                        System.out.println("No users to print!");
-                    }
-                    break;
-                case (3):
-                    if(!Users.isEmpty()) {
-                        System.out.println("Input user id: ");
-                        ID = sc.nextInt();
-                        User userToRemove = null;
-
-                        for (User user : Users) {
-                            if (user.getID() == ID) {
-                                userToRemove = user;
-                                break;
+            if (sc.hasNextInt()) {
+                choice = sc.nextInt();
+                sc.nextLine();
+                switch (choice) {
+                    case (1):
+                        System.out.println("Enter user name: ");
+                        name = sc.nextLine();
+                        if(!name.isEmpty()) {
+                            if(name.matches("[a-zA-Z ]+")) {
+                                System.out.println("Enter user email:");
+                                email = sc.nextLine();
+                                if(!email.isEmpty())
+                                    if(email.contains("@"))
+                                        Users.add(new User(++count, name, email));
+                                    else
+                                        System.out.println("Invalid email! Try again!");
+                                else
+                                    System.out.println("User email needed! Try again!");
+                            }
+                            else{
+                                System.out.println("User name must not include invalid characters! Try again!");
                             }
                         }
-
-                        if (userToRemove != null) {
-                            Users.remove(userToRemove);
-                            System.out.println("User removed successfully.");
-                        } else {
-                            System.out.println("No user found with that ID.");
+                        else{
+                            System.out.println("User name needed! Try again!");
                         }
-                    }
-                    else{
-                        System.out.println("No users to remove!");
-                    }
-                    break;
-                case(4):
-                    exit=true;
-                default:
-                    System.out.println("Invalid option! Try again!");
+                        break;
+                    case (2):
+                        if (!Users.isEmpty()) {
+                            for (User user : Users)
+                                System.out.println(user.toString());
+                        } else {
+                            System.out.println("No users to list!");
+                        }
+                        break;
+                    case (3):
+                        if (!Users.isEmpty()) {
+                            System.out.println("Input user id: ");
+                            if (sc.hasNextInt()) {
+                                ID = sc.nextInt();
+                                sc.nextLine(); // clear buffer
 
+                                User userToRemove = null;
+
+                                for (User user : Users) {
+                                    if (user.getID() == ID) {
+                                        userToRemove = user;
+                                        break;
+                                    }
+                                }
+
+                                if (userToRemove != null) {
+                                    Users.remove(userToRemove);
+                                    System.out.println("User removed successfully.");
+                                } else {
+                                    System.out.println("No user found with that ID.");
+                                }
+                            } else {
+                                System.out.println("ID input has to be a number!");
+                                sc.nextLine(); // clear buffer
+                            }
+                        } else {
+                            System.out.println("No users to remove!");
+                        }
+                        break;
+                    case (4):
+                        exit = true;
+                        break;
+                    default: {
+                        System.out.println("Invalid option! Try again!");
+                        sc.nextLine();
+                    }
+
+                }
+            }
+            else {
+                System.out.println("Please enter a number.");
+                sc.nextLine();
             }
         }
         sc.close();
     }
-
 }
